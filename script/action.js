7ce2f9b8904b75d8ca924c09bea87fa505e5d66f
1215kkm <66404083+1215kkm@users.noreply.github.com>
2024-06-14 18:24:14 +0900

    웹디자인기능사 C-4

---
+++ script/action.js
@@ -0,0 +1,16 @@
+setInterval(function(){
+    $('.slide ul').animate({left:-800},1000, function(){
+        $('.slide li').eq(0).appendTo($('.slide ul'))
+        $('.slide ul').css({left:0})
+    })
+}, 2000)
+
+
+/* popup */
+$('.notice li').eq(0).click(function(){
+    $('.modal').show()
+})
+
+$('.popup a').click(function(){
+    $('.modal').hide()
+})
